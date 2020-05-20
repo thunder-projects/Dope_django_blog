dbae25d1e1a2c87628d3fc736371f5d109c669c9
Yogesh_sirsat <41695888+Mighty-sperm@users.noreply.github.com>
2020-05-20 14:58:02 +0530

    20.05.20 14.57

--- Dope_Django_Blog/settings.py
+++ Dope_Django_Blog/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from decouple import config, Csv
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -104,8 +105,8 @@ WSGI_APPLICATION = 'Dope_Django_Blog.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL'),
-    }
+        default=config('DATABASE_URL')
+    )
 }
 
 
