0e61c7d31278dc7b2354e4af83fe9e78aaea13ae
Yogesh_sirsat <41695888+Mighty-sperm@users.noreply.github.com>
2020-05-20 16:28:56 +0530

    settings changed now

--- Dope_Django_Blog/settings.py
+++ Dope_Django_Blog/settings.py
@@ -104,9 +104,10 @@ WSGI_APPLICATION = 'Dope_Django_Blog.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
